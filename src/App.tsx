c244529a8cc671216e4835f95eda5d3845885053
sp026 <srish.popuri@gmail.com>
2024-08-08 03:12:18 -0400

    FIx App.tsx setInterval
    fixed syntas/setup mistake in setInterval, improperly placed parantheses/brackets

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ class App extends Component<{}, IState> {
    */
   getDataFromServer() {
       let x = 0;
-      const interval = setInterval(){ /*will repeatedly call the following bunch of code */
+      const interval = setInterval(() => { /*will repeatedly call the following bunch of code */
         DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
