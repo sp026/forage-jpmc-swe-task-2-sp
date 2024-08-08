179d177e52fc69cb15e66c23fe59cb7b76f244a3
sp026 <srish.popuri@gmail.com>
2024-08-08 03:14:48 -0400

    Fix app.tsx bracket error (2)
    fix bracket error from function getting data from server

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ class App extends Component<{}, IState> {
             data: serverResponds,
             showGraph: true,
           });
-        }
+        });
        x++;
        if(x>1000){
            clearInterval(interval);
